8db095569d57adfefa5dc863b92b76f4ca94b0ea
Ivan <imikon@proton.me>
2023-12-30 13:54:11 +0100

    added jest

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'xo',
+        'prettier',
+        'plugin:i18next/recommended'
+    ],
+    overrides: [],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['react', '@typescript-eslint', '@stylistic/ts', 'i18next'],
+    rules: {
+        'linebreak-style': ['error', 'windows'],
+        '@typescript-eslint/no-unused-vars': [
+            'warn',
+            { argsIgnorePattern: '^_' },
+        ],
+        '@stylistic/no-tabs': 'off',
+        'react/react-in-jsx-scope': 'off',
+        "i18next/no-literal-string": 2
+    },
+    globals: {
+        __IS_DEV__: true,
+    },
+}
